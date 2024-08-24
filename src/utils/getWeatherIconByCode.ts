877e5b235acc15c2b8606a71a64b1b651ac4191b
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-23 23:30:23 -0300

    refactor: created a function to help when getting weather icons

--- src/utils/getWeatherIconByCode.ts
+++ src/utils/getWeatherIconByCode.ts
@@ -1,20 +1,30 @@
 import TWeatherIconMap from '../types/TWeatherIconMap';
 
-const nightIcons: TWeatherIconMap = {
-    1000: 'clear_night',
-    1003: 'partly_cloudy_night',
+const commonIcons = {
     1063: 'rainy',
     1153: 'rainy',
     1183: 'rainy',
 };
 
-const dayIcons: TWeatherIconMap = {
-    1000: 'clear_day',
-    1003: 'partly_cloudy_day',
-    1063: 'rainy',
-    1153: 'rainy',
-    1183: 'rainy',
-};
+function createWeatherIcons(weatherIconsMap: TWeatherIconMap, defaultIcons?: TWeatherIconMap) {
+    return { ...weatherIconsMap, ...defaultIcons };
+}
+
+const nightIcons: TWeatherIconMap = createWeatherIcons(
+    {
+        1000: 'clear_night',
+        1003: 'partly_cloudy_night',
+    },
+    commonIcons,
+);
+
+const dayIcons: TWeatherIconMap = createWeatherIcons(
+    {
+        1000: 'clear_day',
+        1003: 'partly_cloudy_day',
+    },
+    commonIcons,
+);
 
 function getIcon(code: number, isDay: boolean): string {
     return (isDay ? dayIcons : nightIcons)[code] as string;
